a69513766a6e34763b85cefddf75c63a14ce0dde
nedim begic <nedim.begic93@gmail.com>
2023-11-28 22:45:15 +0100

    adding some style to daily task

--- modules/dailyTask.js
+++ modules/dailyTask.js
@@ -4,4 +4,8 @@ document.getElementById("setTaskBtn").addEventListener("click", function () {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { message: "createTaskForm" });
   });
+  chrome.extension
+    .getBackgroundPage()
+    .chrome.extension.getViews({ type: "popup" })[0]
+    .close();
 });
